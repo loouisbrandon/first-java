3da9f373b915c05aa4327503b0ede2fc4090edbd
Brandon Louis <brandonlouisferreira@gmail.com>
2025-06-04 19:02:33 -0300

    Vê essa merda que ta

--- src/main/java/com/login/DatabaseConnection.java
+++ src/main/java/com/login/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/login_db";
-    private static final String USER = "root";  // ajuste seu usuário
-    private static final String PASSWORD = "";  // ajuste sua senha
+    private static final String USER = "root";  
+    private static final String PASSWORD = "";  //senha? nao temos senha
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
